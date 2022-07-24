aa0bb16905cd3d47ca87b93cd4a419ad879fe415
c_stewart27 <cstewart3@student.sjcny.edu>
2022-07-24 15:04:26 -0400

    Initial Commit

---
+++ mod4Lesson3Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mod4Lesson3Challenge
+//
+//  Created by Christopher Stewart on 7/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mod4Lesson3Challenge/mod4Lesson3ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mod4Lesson3ChallengeApp.swift
+//  mod4Lesson3Challenge
+//
+//  Created by Christopher Stewart on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct mod4Lesson3ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
